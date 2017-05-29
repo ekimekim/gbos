84eee43ea2b1c70b91ba1026ee606d60f59abca8
Mike Lang <mikelang3000@gmail.com>
2017-05-29 04:12:16 -0700

    Add a tool for generating unit test ROMs from python spec files
    and additionally an example spec to test the test tool.

--- Makefile
+++ Makefile
@@ -1,18 +1,25 @@
 
 # avoid implicit rules for clarity
 .SUFFIXES: .asm .o .gb
-.PHONY: bgb clean
+.PHONY: bgb clean tests
 
 ASMS := $(wildcard *.asm)
 OBJS := $(ASMS:.asm=.o)
 INCLUDES := $(wildcard include/*.asm)
 ASSETS := $(shell find assets/ -type f)
+TESTS := $(wildcard tests/*.py)
 
-all: rom.gb
+all: rom.gb tests/.uptodate
 
-include/assets/.uptodate: $(ASSETS)
+include/assets/.uptodate: $(ASSETS) tools/assets_to_asm.py
 	python tools/assets_to_asm.py assets/ include/assets/
 	touch $@
+
+tests/.uptodate: $(TESTS) tools/unit_test_gen.py $(OBJS)
+	python tools/unit_test_gen.py .
+	touch "$@"
+
+tests: tests/.uptodate
 
 %.o: %.asm $(INCLUDES) include/assets/.uptodate
 	rgbasm -i include/ -v -o $@ $<
@@ -25,5 +32,4 @@ bgb: rom.gb
 	bgb $<
 
 clean:
-	rm -f *.o *.sym rom.gb "include/assets/.uptodate"
-
+	rm -f *.o *.sym rom.gb include/assets/.uptodate include/assets/*.asm tests/*/*.{asm,o,sym,gb}
---
+++ tests/meta_test.py
@@ -0,0 +1,67 @@
+"""Example test to test the testing framework"""
+
+# Normally you'd specify a top-level directory .asm file to 'include'.
+# This file should contain the target function. We 'include' it rather than linking it
+# so the test can potentially access unexported ('private') labels.
+# You can say None, but it's a weird case.
+file = None
+
+# What top-level directory .asm files to link with. Default is [], shown here as an example.
+files = []
+
+# function to test
+target = 'TestCode'
+
+# extra asm to add as a helper (should be rare)
+asm = """
+SECTION "Test Mem", WRAM0
+
+TestMem:
+	ds 5
+
+SECTION "Test code", ROM0
+
+; Basic function to test. Should output HL = $face, B = input B + 1,
+; A = TestMem[0] + TestMem[1], TestMem[3] = TestMem[0] - TestMem[1],
+; and set z iff A == TestMem[2]
+TestCode:
+	ld HL, $face
+	inc B
+	ld A, [TestMem+2]
+	ld D, A
+	ld A, [TestMem+1]
+	ld C, A
+	ld A, [TestMem]
+	sub C
+	ld [TestMem+3], A
+	ld A, [TestMem]
+	add C
+	cp D
+	ret
+"""
+
+# anything of type Memory is set as a default initial value for that label in memory
+TestMem = Memory(5, 2, 7)
+
+# Create a test and assign it to a name
+basic = Test(
+	# in_REG sets initial register states
+	in_B = 42,
+	# out_REG checks register states after call
+	out_A = 7,
+	out_HL = 0xface,
+	out_B = 43,
+	# in/out also works for z, c flags
+	out_zflag = True,
+	# out_LABEL checks contents of memory LABEL after call (as long as type is Memory)
+	out_TestMem = Memory(5, 2, 7, 3),
+)
+
+no_match = Test(
+	# We're leaving a lot of things unspecified here since they're tested by basic.
+	# Note the in_LABEL here overrides the overlapping part of the outer TestMem definition
+	in_TestMem = Memory(6),
+	out_zflag = False,
+)
+
+# Currently no way to specify input/output stack, but it wouldn't be much of a change.
